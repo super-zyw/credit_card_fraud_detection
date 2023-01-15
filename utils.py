cda71329f294abd83c044f2f6b1cc0fc4ce76d66
Yinwei Zhang <39347880+yinwei-zhang@users.noreply.github.com>
2023-01-14 21:57:47 -0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,13 +1,60 @@
 import matplotlib.pyplot as plt
-def draw_plot(precision, recall):
-    plt.figure()
-    plt.step(recall, precision, color='r', alpha=0.2,
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import RobustScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+
+def read_data(PATH):
+    df = pd.read_csv(PATH)
+    X, y = df.drop('Class', axis=1), df['Class']
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2, random_state=2023)
+    return Xtrain, Xtest, ytrain, ytest
+
+def get_preprocessor():
+    attributes = ['Amount', 'Time']
+    num_processor = pipeline.Pipeline(steps=([('imputer', SimpleImputer(strategy='median')),
+                                              ('robust_scaler', RobustScaler())]))
+    preprocessor = ColumnTransformer([('num', num_processor, attributes)], remainder='passthrough')
+    return preprocessor
+
+
+def get_model(MODEL_NAME = 'logistic_regression', n_iter = 10):
+    if MODEL_NAME == 'logistic_regression':
+        PARAMS = {'penalty': ['l1', 'l2'],
+                  'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]}
+        MODEL = GridSearchCV(LogisticRegression(solver='liblinear'), PARAMS)
+        # MODEL = RandomizedSearchCV(GradientBoostingClassifier(), PARAMS, n_iter=n_iter)
+
+    elif MODEL_NAME == 'random_forest':
+        PARAMS = {'max_depth': [3, 5, 7, 15],
+                  'n_estimators': [10, 30, 70, 130, 300]}
+        MODEL = GridSearchCV(RandomForestClassifier(), PARAMS)
+        # MODEL = RandomizedSearchCV(GradientBoostingClassifier(), PARAMS, n_iter=n_iter)
+
+    elif MODEL_NAME == 'gradient_boost':
+        PARAMS = {'n_estimators': [10, 30, 70, 130, 300],
+                  'max_depth': [3, 5, 7, 15]}
+        MODEL = GridSearchCV(GradientBoostingClassifier(), PARAMS)
+        #MODEL = RandomizedSearchCV(GradientBoostingClassifier(), PARAMS, n_iter=n_iter)
+    else:
+        raise Exception('Sorry, there is no such model in the database')
+
+    return MODEL
+
+
+def draw_plot(ax, precision, recall):
+    # Draw the precision-recall curve
+    ax.step(recall, precision, color='r', alpha=0.2,
              where='post')
-    plt.fill_between(recall, precision, step='post', alpha=0.2,
+    ax.fill_between(recall, precision, step='post', alpha=0.2,
                      color='#F59B00')
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.ylim([0.0, 1.05])
-    plt.xlim([0.0, 1.0])
-    plt.show()
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlim([0.0, 1.0])
     return
